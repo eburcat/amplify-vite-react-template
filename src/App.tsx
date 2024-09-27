bb074884fe71d072fed1536cd38b84f9f4865add
Eitan <eitan@liveu.tv>
2024-09-27 15:06:33 +0300

    getCurrentUser - 7

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import '@aws-amplify/ui-react/styles.css'
-import { useEffect, useState } from "react";
+//import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 import { signInWithRedirect, getCurrentUser, fetchAuthSession } from 'aws-amplify/auth';
@@ -13,21 +13,21 @@ const client = generateClient<Schema>();
 
 function App() {
 
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  /*const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
     });
-  }, []);
+  }, []);*/
 
   function createTodo() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
 
-  function deleteTodo(id: string) {
+  /*function deleteTodo(id: string) {
     client.models.Todo.delete({id});
-  }
+  }*/
 
   const signInWithOneLogin = async () => {
     console.log("h");
